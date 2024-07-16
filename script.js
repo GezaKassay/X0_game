fc76b065b9b2763413ee8997a4750992dfc28715
GezaKassay <kassay_geza@yahoo.com>
2024-07-16 16:13:16 +0300

    Project completed

--- script.js
+++ script.js
@@ -30,15 +30,22 @@ function updateCellValue() {
     }
     if (clickCounter >= 5) {
         n = 3;
-        checkLines(cellIDs);
-        checkColumns(cellIDs);
-    }
+        m = 3;
+        o = 3;
+        checkLine(lineIDs);
+        checkColumn(columnIDs);
+        checkDiagonal(diagonalIDs);
+    }   
 }
 
-let cellIDs = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let lineIDs = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let columnIDs = ["1", "4", "7", "2", "5", "8", "3", "6", "9"];
+let diagonalIDs = ["1", "5", "9", "7", "5", "3"];
 let n = 3;
+let m = 3;
+let o = 3;
 
-function checkLines(array) {
+function checkLine(array) {
     let matchX = 0;
     let match0 = 0;
     for (let i = 0; i < n; ++i) {
@@ -49,12 +56,12 @@ function checkLines(array) {
             ++match0;
         }
         if (matchX === 3) {            
-            alert(document.getElementById("Player1").innerHTML);
+            document.getElementById("winnerIs").innerHTML = document.getElementById("Player1").innerHTML + " WON!";
         }
         if (match0 === 3) {           
-            alert(document.getElementById("Player2").innerHTML);
+            document.getElementById("winnerIs").innerHTML = document.getElementById("Player2").innerHTML + " WON!";
         }                          
-        if (i === n - 1 && i < cellIDs.length - 1 && !(matchX === 3 || match0 === 3)) {
+        if (i === n - 1 && i < lineIDs.length - 1 && !(matchX === 3 || match0 === 3)) {
             matchX = 0;
             match0 = 0;
             n += 3;
@@ -62,23 +69,50 @@ function checkLines(array) {
     }
 }
 
-let m = 6
-
-function checkColumns(array) {
-    for (let i = 0; i <= ; ++i) {
-        if (document.getElementById(array[i]).innerHTML === "X" &&) {
-            ++matchX;
+function checkColumn(array) {
+    let matchColumnX = 0;
+    let matchColumn0 = 0;
+    for (let i = 0; i < m; ++i) {
+        if (document.getElementById(array[i]).innerHTML === "X") {
+            ++matchColumnX;
         }
         if (document.getElementById(array[i]).innerHTML === "0") {
-            ++match0;
+            ++matchColumn0;
         }
-        if (matchX === 3) {            
-            alert(document.getElementById("Player1").innerHTML);
+        if (matchColumnX === 3) {            
+            document.getElementById("winnerIs").innerHTML = document.getElementById("Player1").innerHTML + " WON!";
         }
-        if (match0 === 3) {           
-            alert(document.getElementById("Player2").innerHTML);
-        }  
-        alert(array[i]);         
+        if (matchColumn0 === 3) {           
+            document.getElementById("winnerIs").innerHTML = document.getElementById("Player2").innerHTML + " WON!";
+        }                          
+        if (i === m - 1 && i < columnIDs.length - 1 && !(matchColumnX === 3 || matchColumn0 === 3)) {
+            matchColumnX = 0;
+            matchColumn0 = 0;
+            m += 3;
+        }           
     }
+}
 
+function checkDiagonal(array) {
+    let matchDiagonalX = 0;
+    let matchDiagonal0 = 0;
+    for (let i = 0; i < o; ++i) {
+        if (document.getElementById(array[i]).innerHTML === "X") {
+            ++matchDiagonalX;
+        }
+        if (document.getElementById(array[i]).innerHTML === "0") {
+            ++matchDiagonal0;
+        }
+        if (matchDiagonalX === 3) {            
+            document.getElementById("winnerIs").innerHTML = document.getElementById("Player1").innerHTML + " WON!";
+        }
+        if (matchDiagonal0 === 3) {           
+            document.getElementById("winnerIs").innerHTML = document.getElementById("Player2").innerHTML + " WON!";
+        }                          
+        if (i === o - 1 && i < diagonalIDs.length - 1 && !(matchDiagonalX === 3 || matchDiagonal0 === 3)) {
+            matchDiagonalX = 0;
+            matchDiagonal0 = 0;
+            o += 3;
+        }           
+    }
 }
