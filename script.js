d4f3180e2111ab6693ae04b9b6796030ee7e3876
GezaKassay <kassay_geza@yahoo.com>
2024-07-29 18:15:22 +0300

    id's saved to matrix

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ function updateName() {
 let elementID;
 let cells = document.getElementsByClassName("col-1");
 
-for (let i = 0; i < cells.length; i++) {
+for (let i = 0; i < cells.length; ++i) {
     cells[i].onclick = updateCellValue;
 }
 
-let checkIDs = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "1", "4", "7", "2",     
-    "5", "8", "3", "6", "9", "1", "5", "9", "7", "5", "3"];
-let idSet = 3;
+let idMatrix = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["1", "4",
+    "7"], ["2", "5", "8"], ["3", "6", "9"], ["1", "5", "9"], ["7", "5", "3"]];
+let matrixColumns = 3;
 
 function updateCellValue() {    
     elementID = this.id;
@@ -31,35 +31,30 @@ function updateCellValue() {
     } else {
         document.getElementById(elementID).innerHTML = "0";
     }
-    if (clickCounter >= 5) {
-        idSet = 3;       
-        checkForWinner(checkIDs);        
+    if (clickCounter >= 5) {          
+        checkForWinner(idMatrix);        
     }   
 }
 
-function checkForWinner(array) {
-    let matchX = 0;
-    let match0 = 0;
-    for (let i = 0; i < idSet; ++i) {
-        if (document.getElementById(array[i]).innerHTML === "X") {
-            ++matchX;
-        }
-        if (document.getElementById(array[i]).innerHTML === "0") {
-            ++match0;
-        }
-        if (matchX === 3) {            
-            document.getElementById("winnerIs").innerHTML = 
-                document.getElementById("Player1").innerHTML + " WON!";
-        }
-        if (match0 === 3) {           
-            document.getElementById("winnerIs").innerHTML = 
-                document.getElementById("Player2").innerHTML + " WON!";
-        }                          
-        if (i === idSet - 1 && i < checkIDs.length - 1 && 
-            !(matchX === 3 || match0 === 3)) {
-            matchX = 0;
-            match0 = 0;
-            idSet += 3;
+function checkForWinner(array) {    
+    for (let i = 0; i < idMatrix.length; ++i) {
+        let matchX = 0;
+        let match0 = 0;
+        for (let j = 0; j < matrixColumns; ++j) {
+            if (document.getElementById(array[i][j]).innerHTML === "X") {
+                ++matchX;
+            }
+            if (document.getElementById(array[i][j]).innerHTML === "0") {
+                ++match0;
+            }
+            if (matchX === 3) {
+                document.getElementById("winnerIs").innerHTML = 
+                    document.getElementById("Player1").innerHTML + " WON!";
+            }
+            if (match0 === 3) {
+                document.getElementById("winnerIs").innerHTML = 
+                    document.getElementById("Player2").innerHTML + " WON!";
+            }                          
         }           
     }
 }
